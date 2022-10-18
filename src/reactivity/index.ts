2accba0ba7ab04a9829b890b8811e2b16235ea80
itw_yuwb <itw_yuwb@tkmail.com>
2022-10-18 22:39:01 +0800

    🎉 init(Test环境): 初始化测试环境

---
+++ src/reactivity/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Date: 2022-10-18 22:15:01
+ * @LastEditTime: 2022-10-18 22:25:41
+ * @Description:
+ *  a,b ：TS语法报错，可以配置tsconfig.json中noImplicitAny为false
+ */
+export function add(a,b){
+  return a + b
+}
---
+++ src/reactivity/tests/index.spec.ts
@@ -0,0 +1,9 @@
+/*
+ * @Date: 2022-10-18 22:15:35
+ * @LastEditTime: 2022-10-18 22:27:59
+ * @Description:  检测测试
+ */
+import { add } from '../index'
+it('init', () => {
+  expect(add(1,1)).toBe(2)
+});
