64259517a30bee299c1592a77bcd04ac3feb4bc4
itw_yuwb <itw_yuwb@tkmail.com>
2022-10-20 22:36:17 +0800

    ✨ feat(reactivity): 添加部分注释

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,6 +1,6 @@
 /*
  * @Date: 2022-10-18 22:47:44
- * @LastEditTime: 2022-10-20 14:31:56
+ * @LastEditTime: 2022-10-20 22:36:10
  * @Description: 
  */
 import { reactive } from '../reactive'
@@ -8,7 +8,7 @@ import { reactive } from '../reactive'
 // 尽量每次编写测试用例都用describe包裹进行分块
 // 每个测试用例一个it函数代表
 // 参数：
-// - 字符串，代表测试用例名称：常用命名模式“被测对象在什么情况下是什么行为
+// - 字符串，代表测试用例名称：常用命名模式“被测对象在什么情况下是什么行为”
 // - 函数，实际测试用例过程
 
 describe('reactive', () => { 
